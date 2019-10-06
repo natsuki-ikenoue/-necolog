4b290fb3b293ff2feb5b1380894efa51914c267e
natsuki.ikenoue <tsumire617@gmail.com>
2019-10-06 01:11:41 +0000

    image square

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,10 +33,28 @@ class UsersController < ApplicationController
     counts(@user)
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        render :edit
+      else
+        flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end   
+    else
+      redirect_to root_url
+    end
+  end
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create] do
+  resources :users, only: [:index, :show, :new, :create, :edit, :update] do
     member do
       get :likes
     end
